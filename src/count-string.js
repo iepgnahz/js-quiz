2d6fabd516afb8425d8167a90abb8f299880371d
Yvette14 <libra1014tyb@163.com>
2017-03-04 11:54:17 +0800

    complete answer function

--- src/count-string.js
+++ src/count-string.js
@@ -1,7 +1,27 @@
 function countString(collection) {
   //在这里写入代码
 
+  var result = [];
+  var number = [];
+  collection.forEach(function (charactor) {
+    var index = result.indexOf(charactor);
+    if (index === -1) {
+      result.push(charactor);
+    }
+  });
 
+  result.forEach(function (item) {
+    var count = 0;
+    for (key in collection) {
+      count = (collection[key] === item) ? ++count : count;
+    }
+    number.push(count);
+  });
+
+  result = result.map(function (item, index) {
+    return item + '-' + number[index];
+  });
+  return result;
 }
 
 module.exports = countString;
